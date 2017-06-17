b269589890cf76e8911495102b735a90c21333a1
Witkowski <matthew.witkowski@G3TI.Local>
2017-06-17 00:32:17 -0400

    Fixed a bug in the myMap signature, and made the base case more readable.

--- ch04/Map.hs
+++ ch04/Map.hs
@@ -13,3 +13,7 @@ square2 xs = map squareOne xs
     where squareOne x = x * x
 
 upperCase2 xs = map toUpper xs
+
+myMap :: (a -> b) -> [a] -> [b]
+myMap f (x:xs) = f x : myMap f xs
+myMap _ [] = []
