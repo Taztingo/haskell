b2924777fd9ba1d5cfb8e82372485a80b5d64624
Witkowski <matthew.witkowski@G3TI.Local>
2017-06-21 08:43:02 -0400

    Created a suffix tree in haskell.

---
+++ ch04/SuffixTree.hs
@@ -0,0 +1,12 @@
+-- file: ch04/SuffixTree.hs
+import Data.List
+
+suffixes :: [a] -> [[a]]
+suffixes xs@(_:xs') = xs : suffixes xs'
+suffixes _ = []
+
+noAsPattern :: [a] -> [[a]]
+noAsPattern (x:xs) = (x:xs) : noAsPattern xs
+noAsPattern _ = []
+
+suffixes2 xs = init (tails xs)
