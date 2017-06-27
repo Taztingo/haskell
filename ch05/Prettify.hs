d26549285b70e3189a4e9e9e4ac523d4432e6665
Witkowski <matthew.witkowski@G3TI.Local>
2017-06-27 08:21:56 -0400

    Fixed indentation for Prettify.hs

--- ch05/Prettify.hs
+++ ch05/Prettify.hs
@@ -85,18 +85,20 @@ punctuate p (d:ds) = (d <> p) : punctuate p ds
 
 pretty :: Int -> Doc -> String
 pretty width x = best 0 [x]
-    where best col (d:ds) = case d of
+    where 
+        best col (d:ds) = case d of
             Empty -> best col ds
             Char c -> c : best (col + 1) ds
             Text s -> s ++ best (col + length s) ds
             Line -> '\n' : best 0 ds
             a `Concat` b -> best col (a:b:ds)
             a `Union` b -> nicest col (best col (a:ds)) (best col (b:ds))
-best _ _ = ""
+        best _ _ = ""
 
-    nicest col a b | (width - least) `fits` a = a
-                   | otherwise = b
-                where least = min width col
+        nicest col a b | (width - least) `fits` a = a
+                       | otherwise = b
+            where
+                least = min width col
 
 fits :: Int -> String -> Bool
 w `fits` _ | w < 0 = False
